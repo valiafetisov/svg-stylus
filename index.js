2480f5263e340af516f4035491ab62e9fad42e34
valia fetisov <valyalof@gmail.com>
2015-09-01 03:15:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
 var rework = require('rework');
 var svg = require('./svg');
 
-module.exports = function () {
+module.exports = function (slug) {
   var args = Array.prototype.slice.call(arguments);
+  console.log('args', args, args.pop());
 
   return function (style) {
     this.on('end', function (err, css) {
       var cssObj = rework(css);
-      cssObj.use(svg(args[0]));
+      cssObj.use(svg(slug));
 
       return cssObj.toString();
     });
